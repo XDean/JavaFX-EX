52ae028db57f0b30f0098777ff53da5b5df27c68
XDean <373216024@qq.com>
2017-12-01 10:02:38 +0800

    remove @UtilityClass, deploy to oss central

--- src/main/java/xdean/jfx/ex/util/bean/BeanUtil.java
+++ src/main/java/xdean/jfx/ex/util/bean/BeanUtil.java
@@ -3,8 +3,9 @@ package xdean.jfx.ex.util.bean;
 import static xdean.jex.util.task.TaskUtil.firstSuccess;
 import static xdean.jfx.ex.util.bean.BeanConvertUtil.normalize;
 
-import java.lang.reflect.Method;
 import java.util.function.Function;
+
+import com.google.common.annotations.Beta;
 
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.ObjectBinding;
@@ -13,19 +14,14 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import lombok.experimental.UtilityClass;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
 import xdean.jex.util.cache.CacheUtil;
 import xdean.jex.util.lang.ExceptionUtil;
 
-import com.google.common.annotations.Beta;
-
-@UtilityClass
 public class BeanUtil {
 
-  public BooleanBinding isNull(final ObservableValue<?> op) {
+  public static BooleanBinding isNull(final ObservableValue<?> op) {
     if (op == null) {
       throw new NullPointerException("Operand cannot be null.");
     }
@@ -52,11 +48,11 @@ public class BeanUtil {
     };
   }
 
-  public BooleanBinding isNotNull(final ObservableValue<?> op) {
+  public static BooleanBinding isNotNull(final ObservableValue<?> op) {
     return not(isNull(op));
   }
 
-  public BooleanBinding yep(final ObservableValue<Boolean> ov) {
+  public static BooleanBinding yep(final ObservableValue<Boolean> ov) {
     if (ov == null) {
       throw new NullPointerException("Operand cannot be null.");
     }
@@ -83,7 +79,7 @@ public class BeanUtil {
     };
   }
 
-  public BooleanBinding not(final ObservableValue<Boolean> ov) {
+  public static BooleanBinding not(final ObservableValue<Boolean> ov) {
     if (ov == null) {
       throw new NullPointerException("Operand cannot be null.");
     }
@@ -110,7 +106,7 @@ public class BeanUtil {
     };
   }
 
-  public <F, T> Property<T> nestProp(ObservableValue<F> pf, Function<F, Property<T>> func) {
+  public static <F, T> Property<T> nestProp(ObservableValue<F> pf, Function<F, Property<T>> func) {
     return new SimpleObjectProperty<T>() {
       {
         pf.addListener((ob, o, n) -> {
@@ -126,7 +122,7 @@ public class BeanUtil {
     };
   }
 
-  public <F, T> ObservableValue<T> nestValue(ObservableValue<F> pf, Function<F, ObservableValue<T>> func) {
+  public static <F, T> ObservableValue<T> nestValue(ObservableValue<F> pf, Function<F, ObservableValue<T>> func) {
     return new SimpleObjectProperty<T>() {
       {
         pf.addListener((ob, o, n) -> {
@@ -144,43 +140,40 @@ public class BeanUtil {
 
   @Beta
   @SuppressWarnings("unchecked")
-  <T> T nestWrap(ObservableValue<T> p, Class<T> clz) {
+  public static <T> T nestWrap(ObservableValue<T> p, Class<T> clz) {
     Enhancer enhancer = new Enhancer();
     enhancer.setSuperclass(clz);
-    enhancer.setCallback(new MethodInterceptor() {
-      @Override
-      public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-        if (method.getName().endsWith("Property")) {
-          Object result = firstSuccess(
-              () -> CacheUtil.cache(
-                  p,
-                  method.toString(),
-                  () -> normalize(nestProp(p, t -> ExceptionUtil.uncheck(() -> (Property<Object>) method.invoke(t, args))),
-                      method.getReturnType())),
-              () -> CacheUtil.cache(p, method.toString(),
-                  () -> nestValue(p, t -> ExceptionUtil.uncheck(() -> (ObservableValue<Object>) method.invoke(t, args)))));
-          if (result != null) {
-            return result;
-          }
+    enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+      if (method.getName().endsWith("Property")) {
+        Object result = firstSuccess(
+            () -> CacheUtil.cache(
+                p,
+                method.toString(),
+                () -> normalize(nestProp(p, t1 -> ExceptionUtil.uncheck(() -> (Property<Object>) method.invoke(t1, args))),
+                    method.getReturnType())),
+            () -> CacheUtil.cache(p, method.toString(),
+                () -> nestValue(p, t2 -> ExceptionUtil.uncheck(() -> (ObservableValue<Object>) method.invoke(t2, args)))));
+        if (result != null) {
+          return result;
         }
-        return proxy.invokeSuper(obj, args);
       }
+      return proxy.invokeSuper(obj, args);
     });
     return (T) enhancer.create();
   }
 
-  public interface MapableValue<T> extends ObservableValue<T> {
+  public static interface MapableValue<T> extends ObservableValue<T> {
     public <P> MapableValue<P> map(Function<T, P> func);
   }
 
-  private abstract class SimpleMapableValue<T> extends ObjectBinding<T> implements MapableValue<T> {
+  private static abstract class SimpleMapableValue<T> extends ObjectBinding<T> implements MapableValue<T> {
     @Override
     public <P> MapableValue<P> map(Function<T, P> func) {
       return BeanUtil.map(this, func);
     }
   }
 
-  public <F, T> MapableValue<T> map(ObservableValue<F> ov, Function<F, T> func) {
+  public static <F, T> MapableValue<T> map(ObservableValue<F> ov, Function<F, T> func) {
     return new SimpleMapableValue<T>() {
       {
         bind(ov);
@@ -191,23 +184,5 @@ public class BeanUtil {
         return ExceptionUtil.uncatch(() -> func.apply(ov.getValue()));
       };
     };
-  }
-
-  /**
-   * @deprecated Use {@link CollectionUtil#map(ObservableList<F>,Function<F, T>,Function<T, F>)} instead
-   */
-  @Deprecated
-  public <F, T> ObservableList<T> map(ObservableList<F> list, Function<F, T> forward, Function<T, F> backward) {
-    return CollectionUtil.map(list, forward, backward);
-  }
-
-  /**
-   * @deprecated Use {@link CollectionUtil#bind(ObservableList<F>,ObservableList<T>,Function<F, T>,Function<T, F>)}
-   *             instead
-   */
-  @Deprecated
-  public <F, T> void bind(ObservableList<F> list1, ObservableList<T> list2, Function<F, T> forward,
-      Function<T, F> backward) {
-    CollectionUtil.bind(list1, list2, forward, backward);
   }
 }
