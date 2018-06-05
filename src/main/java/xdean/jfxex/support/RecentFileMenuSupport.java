8ec0bb9b2ce5c8b92a3c9e8c85710be46b74c874
XDean <373216024@qq.com>
2018-06-05 09:35:32 +0800

    refactor RecentFileMenuSupport

--- src/main/java/xdean/jfxex/support/RecentFileMenuSupport.java
+++ src/main/java/xdean/jfxex/support/RecentFileMenuSupport.java
@@ -1,46 +1,46 @@
 package xdean.jfxex.support;
 
-import java.io.File;
-import java.nio.file.Files;
+import static xdean.jex.util.function.Predicates.isEquals;
+import static xdean.jfxex.bean.BeanUtil.mapList;
+import static xdean.jfxex.bean.ListenerUtil.list;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import io.reactivex.Observable;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
-import javafx.util.Pair;
 import xdean.jex.log.Logable;
 
-public abstract class RecentFileMenuSupport implements Logable{
+public abstract class RecentFileMenuSupport implements Logable {
 
-  private ObservableList<Pair<File, MenuItem>> recents;
-  private Consumer<File> onAction = f -> getClass();
-  @SuppressWarnings("unused")
-  private Menu menu;
+  private final ObservableList<Path> recentFiles = FXCollections.observableArrayList();
 
   public RecentFileMenuSupport(Menu menu) {
-    this.menu = menu;
-    recents = FXCollections.observableArrayList();
-    recents.addListener((ListChangeListener.Change<? extends Pair<File, MenuItem>> change) -> {
-      while (change.next()) {
-        if (change.wasRemoved()) {
-          change.getRemoved().stream()
-              .map(Pair::getValue)
-              .forEach(menu.getItems()::remove);
-        }
-        else if (change.wasAdded()) {
-          change.getAddedSubList().stream()
-              .map(Pair::getValue)
-              .forEach(item -> menu.getItems().add(0, item));
-        }
-        _save();
-      }
-    });
     _load();
+    recentFiles.addListener(list(b -> b.onChange(c -> _save())));
+  }
+
+  public void bind(Menu menu, Consumer<Path> onAction) {
+    bind(menu, o -> {
+      MenuItem item = new MenuItem();
+      item.setText(o.toString());
+      item.setOnAction(e -> onAction.accept(o));
+      return item;
+    });
+  }
+
+  public void bind(Menu menu, Function<Path, MenuItem> factory) {
+    ObservableList<MenuItem> items = mapList(recentFiles, factory);
+    Bindings.bindContentBidirectional(items, menu.getItems());
   }
 
   public abstract List<String> load();
@@ -52,59 +52,29 @@ public abstract class RecentFileMenuSupport implements Logable{
       Observable.fromIterable(load())
           .map(String::trim)
           .filter(s -> s.length() != 0)
-          .map(File::new)
-          .map(this::getRecentMenuItemPair)
-          .forEach(recents::add);
+          .map(Paths::get)
+          .forEach(recentFiles::add);
     } catch (Exception e) {
       error().log("Error to load recent location", e);
     }
   }
 
   private void _save() {
-    save(recents.stream()
-        .map(Pair::getKey)
-        .map(File::toString)
+    save(recentFiles.stream()
+        .map(Path::toString)
         .collect(Collectors.toList()));
   }
 
-  public File getLastFile() {
-    if (recents.size() == 0) {
-      return new File(".");
-    } else {
-      Pair<File, MenuItem> pair = recents.remove(recents.size() - 1);
-      File file = pair.getKey();
-      if (Files.exists(file.toPath())) {
-        return file;
-      } else {
-        recents.remove(pair);
-        return getLastFile();
-      }
-    }
+  public Optional<Path> getLatestFile() {
+    return recentFiles.stream().findFirst();
   }
 
-  public void setLastFile(File file) {
-    if (file == null) {
-      return;
-    }
-    List<Pair<File, MenuItem>> collect = recents.stream().filter(p -> p.getKey().equals(file))
-        .collect(Collectors.toList());
-    recents.removeAll(collect);
-    recents.add(getRecentMenuItemPair(file));
+  public void setLastestFile(Path path) {
+    recentFiles.removeIf(isEquals(path));
+    recentFiles.add(0, path);
   }
 
   public void clear() {
-    recents.clear();
+    recentFiles.clear();
   }
-
-  public void setOnAction(Consumer<File> onAction) {
-    this.onAction = onAction;
-  }
-
-  private Pair<File, MenuItem> getRecentMenuItemPair(File file) {
-    MenuItem item = new MenuItem();
-    item.setText(file.toString());
-    item.setOnAction(e -> onAction.accept(file));
-    return new Pair<>(file, item);
-  }
-
 }
