3f6f34881cc7d81278173eadf43791c0e6a85a42
XDean <373216024@qq.com>
2018-06-08 16:57:25 +0800

    add DebugUtil.trace

--- src/main/java/xdean/jfxex/util/DebugUtil.java
+++ src/main/java/xdean/jfxex/util/DebugUtil.java
@@ -1,9 +1,16 @@
 package xdean.jfxex.util;
 
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 
 public class DebugUtil {
+
+  public static void traceBean(ObservableValue<?> ov) {
+    ov.addListener((ob, o, n) -> System.out
+        .println(String.format("Observable: %s changed at %s, from %s to %s", ob, Thread.currentThread(), o, n)));
+  }
+
   public static void printAllWithId(Node n, int space) {
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < space; i++) {
