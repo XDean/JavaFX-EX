75acd9368f54cda8e47107ab7f4a4cc28a6f6a28
XDean <373216024@qq.com>
2018-06-25 14:18:38 +0800

    update RecentFileSupport

--- src/main/java/xdean/jfxex/support/RecentFileMenuSupport.java
+++ src/main/java/xdean/jfxex/support/RecentFileMenuSupport.java
@@ -1,59 +1,58 @@
 package xdean.jfxex.support;
 
-import static xdean.jex.util.cache.CacheUtil.set;
 import static xdean.jex.util.function.Predicates.isEquals;
 import static xdean.jfxex.bean.BeanUtil.mapList;
 import static xdean.jfxex.bean.ListenerUtil.list;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
-
 import io.reactivex.Observable;
-import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import xdean.jex.log.Logable;
 
 public abstract class RecentFileMenuSupport implements Logable {
 
   private final ObservableList<Path> recentFiles = FXCollections.observableArrayList();
+  private final List<Consumer<Path>> handlers = new ArrayList<>();
 
   public RecentFileMenuSupport() {
-    recentFiles.addListener(list(b -> b.onChange(c -> doSave())));
+    recentFiles.addListener(list(b -> b.onChange(c -> save())));
   }
 
-  public void bind(Menu menu, Consumer<Path> onAction) {
-    bind(menu, o -> {
-      MenuItem item = new MenuItem();
-      item.setText(o.toString());
-      item.setOnAction(e -> onAction.accept(o));
-      return item;
-    });
+  public ObservableList<MenuItem> toMenuItems() {
+    return mapList(recentFiles, this::pathToMenuItem);
   }
 
-  public void bind(Menu menu, Function<Path, MenuItem> factory) {
-    ObservableList<MenuItem> items = mapList(recentFiles, factory);
-    set(menu, "recentList", items);
-    Bindings.bindContentBidirectional(menu.getItems(), items);
+  public void addHandler(Consumer<Path> handler) {
+    handlers.add(handler);
   }
 
-  public abstract List<String> load();
+  public void removeHandler(Consumer<Path> handler) {
+    handlers.remove(handler);
+  }
 
-  public abstract void save(List<String> s);
+  protected MenuItem pathToMenuItem(Path path) {
+    MenuItem item = new MenuItem();
+    item.setText(path.toString());
+    item.setOnAction(e -> handlers.forEach(h -> h.accept(path)));
+    return item;
+  }
 
-  @PostConstruct
-  protected void doLoad() {
+  protected abstract List<String> doLoad();
+
+  protected abstract void doSave(List<String> s);
+
+  public void load() {
     try {
-      Observable.fromIterable(load())
+      Observable.fromIterable(doLoad())
           .map(String::trim)
           .filter(s -> s.length() != 0)
           .map(Paths::get)
@@ -63,8 +62,8 @@ public abstract class RecentFileMenuSupport implements Logable {
     }
   }
 
-  protected void doSave() {
-    save(recentFiles.stream()
+  public void save() {
+    doSave(recentFiles.stream()
         .map(Path::toString)
         .collect(Collectors.toList()));
   }
