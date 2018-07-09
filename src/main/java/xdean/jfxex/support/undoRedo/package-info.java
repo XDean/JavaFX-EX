52bbdcf02147b20394c77ffc59c20bfc265e92f7
XDean <373216024@qq.com>
2018-07-09 10:45:46 +0800

    update codecov

---
+++ src/main/java/xdean/jfxex/support/undoRedo/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author Dean Xu (XDean@github.com)
+ *
+ */
+@Beta
+@CodecovIgnore
+package xdean.jfxex.support.undoRedo;
+
+import io.reactivex.annotations.Beta;
+import xdean.codecov.CodecovIgnore;
